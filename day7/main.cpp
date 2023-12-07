08da7e49ab4f3dfcdc4657af226eea940aa7956a
amazzetta <aaron.mazzetta@gmail.com>
2023-12-07 13:38:21 +0100

    solved, now cleanup

--- day7/main.cpp
+++ day7/main.cpp
@@ -17,9 +17,9 @@ enum hand_type {
 hand_type get_type_p1(const std::string& hand)
 {
     std::map<char, int> card_count;
-    for ( char card : hand ) {
+    std::for_each(hand.begin(), hand.end(), [&card_count] (char card) {
         card_count[card]++;
-    }
+    });
 
     int pairs = 0;
     bool three = false, four = false, five = false;
@@ -42,166 +42,10 @@ hand_type get_type_p1(const std::string& hand)
 
 hand_type get_type_p2(const std::string& hand)
 {
-    hand_type type1 = get_type_p1(hand);
-
-    int jokers = 0;
-    for ( char card : hand ) {
-        if ( card == 'J' ) ++jokers;
-    }
-
-    if ( jokers == 0 ) return type1;
-
-    if ( jokers == 1 ) {
-        if ( type1 == high_card ) return one_pair;
-        if ( type1 == one_pair ) return three_of_a_kind;
-        if ( type1 == two_pair ) return full_house;
-        if ( type1 == three_of_a_kind ) return four_of_a_kind;
-        if ( type1 == full_house ) return four_of_a_kind;
-        if ( type1 == four_of_a_kind ) return five_of_a_kind;
-    }
-
-    if ( jokers == 2 ) {
-        if ( type1 == one_pair ) return three_of_a_kind;
-        if ( type1 == two_pair ) return four_of_a_kind;
-        if ( type1 == full_house ) return five_of_a_kind;
-    }
-
-    if ( jokers == 3 ) {
-        if ( type1 == three_of_a_kind ) return four_of_a_kind;
-        if ( type1 == full_house ) return five_of_a_kind;
-    }
-
-    if ( jokers == 4 ) {
-        if ( type1 == four_of_a_kind ) return five_of_a_kind;
-    }
-}
-
-struct input {
-    std::string hand;
-    int bid;
-
-    input(const std::string& in)
-        : hand(in.substr(0, in.find(" "))),
-        bid(std::stoi(in.substr(in.find(" ") + 1)))
-    { }
-
-    friend std::ostream& operator<<(std::ostream& os, const input& in)
-    {
-        return os << in.hand << " " << in.bid;
-    }
-
-    bool comparator_p1(const input& rhs) const
-    {
-        hand_type lhs_type = get_type_p1(hand);
-        hand_type rhs_type = get_type_p1(rhs.hand);
-        if ( lhs_type != rhs_type ) return lhs_type < rhs_type;
-
-        const static std::string card_val = "23456789TJQKA";
-        for ( int i = 0; i < hand.size(); ++i ) {
-            if ( hand[i] == rhs.hand[i] ) continue;
-            return card_val.find(hand[i]) < card_val.find(rhs.hand[i]);
-        }
-
-        return false;
-    }
-
-    bool comparator_p2(const input& rhs) const
-    {
-        hand_type lhs_type = get_type_p2(hand);
-        hand_type rhs_type = get_type_p2(rhs.hand);
-        if ( lhs_type != rhs_type ) return lhs_type < rhs_type;
-
-        const static std::string card_val = "23456789TJQKA";
-        for ( int i = 0; i < hand.size(); ++i ) {
-            if ( hand[i] == rhs.hand[i] ) continue;
-            return card_val.find(hand[i]) < card_val.find(rhs.hand[i]);
-        }
-
-        return false;
-    }
-};
-
-std::vector<input> get_input(const std::string& path)
-{
-    std::fstream file(path, std::ios::in);
-    if ( !file.is_open() ) {
-        std::cout << "failed to open file\n";
-        return {};
-    }
-
-    std::vector<input> inputs;
-    std::string line;
-    while ( std::getline(file, line) ) {
-        inputs.emplace_back(line);
-    }
-
-    return inputs;
-}
-
-void p1(const std::string& path)
-{
-    auto inputs = get_input(path);
-
-    std::sort(inputs.begin(), inputs.end(), [ ] (const input& lhs, const input& rhs) {
-        return lhs.comparator_p1(rhs);
-    });
-
-    long long sum = 0;
-    int n = inputs.size();
-    for ( int i = n-1; i >= 0; --i ) {
-        sum += (i+1) * inputs[i].bid;
-    }
-
-    if ( sum != 250232501 )
-        std::cout << "wrong answer\n";
-    else
-        std::cout << "p1: " << sum << '\n';
-}
-
-void p2(const std::string& path)
-{
-    auto inputs = get_input(path);
-
-    std::sort(inputs.begin(), inputs.end(), [ ] (const input& lhs, const input& rhs) {
-        return lhs.comparator_p2(rhs);
-    });
-
-    long long sum = 0;
-    int n = inputs.size();
-    for ( int i = n-1; i >= 0; --i ) {
-        sum += (i+1) * inputs[i].bid;
-    }
-
-    std::cout << "p2: " << sum;
-    if ( sum <= 248845051 ) {
-        std::cout << " too low";
-    }
-    else if ( sum >= 249292613 ) {
-        std::cout << " too high";
-    }
-    else if ( sum == 249256823 ) {
-        std::cout << " wrong";
-    }
-    else if ( sum == 249242039 ) {
-        std::cout << " wrong";
-    }
-
-    std::cout << '\n';
-}
-
-int main()
-{
-    const std::string input = "input.txt";
-    p1(input);
-    p2(input);
-}
-
-
-/*
-std::map<char, int> card_count;
-    for ( char card : hand ) {
+    std::map<char, int> card_count;
+    std::for_each(hand.begin(), hand.end(), [&card_count] (char card) {
         card_count[card]++;
-    }
+    });
 
     int jokers = card_count['J'];
     if ( jokers == 0 ) return get_type_p1(hand);
@@ -211,16 +55,15 @@ std::map<char, int> card_count;
 
     int pairs = 0;
     bool three = false, four = false, five = false;
-
     for ( auto const& pair : card_count ) {
         if ( pair.first == 'J' ) {
             continue;
         }
+
         else if ( pair.second == 2 ) pairs++;
         else if ( pair.second == 3 ) three = true;
         else if ( pair.second == 4 ) four = true;
     }
-
 
     if ( jokers == 3 ) {
         if ( pairs == 1 ) return five_of_a_kind;
@@ -238,4 +81,106 @@ std::map<char, int> card_count;
         else if ( pairs == 1 ) return three_of_a_kind;
         else return one_pair;
     }
-*/
+}
+
+struct Input {
+    std::string hand;
+    int bid;
+
+    Input(const std::string& in)
+        : hand(in.substr(0, in.find(" "))),
+        bid(std::stoi(in.substr(in.find(" ") + 1)))
+    { }
+
+    bool comparator_p1(const Input& rhs) const
+    {
+        hand_type lhs_type = get_type_p1(hand);
+        hand_type rhs_type = get_type_p1(rhs.hand);
+        if ( lhs_type != rhs_type ) return lhs_type < rhs_type;
+
+        const static std::string card_val = "23456789TJQKA";
+        for ( int i = 0; i < hand.size(); ++i ) {
+            if ( hand[i] == rhs.hand[i] ) continue;
+            return card_val.find(hand[i]) < card_val.find(rhs.hand[i]);
+        }
+
+        return false;
+    }
+
+    bool comparator_p2(const Input& rhs) const
+    {
+        hand_type lhs_type = get_type_p2(hand);
+        hand_type rhs_type = get_type_p2(rhs.hand);
+        if ( lhs_type != rhs_type ) return lhs_type < rhs_type;
+
+        const static std::string card_val = "J23456789TQKA";
+        for ( int i = 0; i < hand.size(); ++i ) {
+            if ( hand[i] == rhs.hand[i] ) continue;
+            return card_val.find(hand[i]) < card_val.find(rhs.hand[i]);
+        }
+
+        return false;
+    }
+};
+
+std::vector<Input> get_input(const std::string& path)
+{
+    std::fstream file(path, std::ios::in);
+    if ( !file.is_open() ) {
+        std::cout << "failed to open file\n";
+        return {};
+    }
+
+    std::vector<Input> inputs;
+    std::string line;
+    while ( std::getline(file, line) )
+        inputs.emplace_back(line);
+
+    return inputs;
+}
+
+long long p1(std::vector<Input>& input)
+{
+    std::sort(input.begin(), input.end(), [ ] (const Input& lhs, const Input& rhs) {
+        return lhs.comparator_p1(rhs);
+    });
+
+    long long sum = 0;
+    int n = input.size();
+    for ( int i = 0; i < n; ++i ) {
+        sum += (i+1) * input[i].bid;
+    }
+
+    return sum;
+}
+
+long long p2(std::vector<Input>& input)
+{
+    std::sort(input.begin(), input.end(), [ ] (const Input& lhs, const Input& rhs) {
+        return lhs.comparator_p2(rhs);
+    });
+
+    long long sum = 0;
+    int n = input.size();
+    for ( int i = 0; i < n; ++i ) {
+        sum += (i+1) * input[i].bid;
+    }
+
+    return sum;
+}
+
+int main()
+{
+    const std::string input = "input.txt";
+    std::vector<Input> inputs = get_input(input);
+    long long ans1 = p1(inputs);
+    long long ans2 = p2(inputs);
+
+    if ( ans1 != 250232501 || ans2 != 249138943 ) {
+        std::cout << "you fucked up\n";
+        return 1;
+    }
+
+    std::cout << "p1: " << p1(inputs) << '\n';
+    std::cout << "p2: " << p2(inputs) << '\n';
+}
